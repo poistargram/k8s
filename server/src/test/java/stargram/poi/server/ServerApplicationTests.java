eb0bc5d11b3052bb9ae23ddd20aceb207d879dd0
poistargram <poi.stargram@gamil.com>
2023-03-25 19:27:30 +0900

    [0] 쿠버네티스 4번째이자 위대한 마지막도전
    2023년 03월 25일

---
+++ server/src/test/java/stargram/poi/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package stargram.poi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
