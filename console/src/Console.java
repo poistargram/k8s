4274cf0f66cede877b254dff56b62d3577074413
poistargram <poi.stargram@gamil.com>
2023-03-31 14:23:09 +0900

    [4] 크론잡

---
+++ console/src/Console.java
@@ -0,0 +1,12 @@
+public class Console {
+
+	public static void main(String[] args) {
+		try {
+			Thread.sleep(20000);
+		}catch (Exception e){
+			System.out.println("Hello, Error!");
+		}
+
+	}
+
+}
